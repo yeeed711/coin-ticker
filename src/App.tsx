2a32bb84ef4cb68e74554a577eaa5dcd8eeb819a
yeeed711 <rladpwl910@naver.com>
2022-09-28 00:38:52 +0900

    feat: darktheme 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './GlobalStyle';
 import Router from './routes/Router';
-import { theme } from './theme';
+import { darkTheme, lightTheme } from './theme';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={darkTheme}>
       <GlobalStyle />
       <Router />
     </ThemeProvider>
--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,13 @@
 import { DefaultTheme } from 'styled-components';
 
-export const theme: DefaultTheme = {
+export const lightTheme: DefaultTheme = {
   bgColor: 'whitesomke',
   textColor: 'black',
+  accentColor: '#fdad32',
+};
+
+export const darkTheme: DefaultTheme = {
+  bgColor: '#141519',
+  textColor: '#aab1b9',
   accentColor: '#fdad32',
 };
