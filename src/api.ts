51a9f5208f97e07066ba8336a09225aa96007085
yeeed711 <rladpwl910@naver.com>
2022-09-29 18:52:51 +0900

    feat: 기존 api를 bithumb api로 변경

--- src/api.ts
+++ src/api.ts
@@ -1,15 +1,3 @@
-const BASE_URL = `https://api.coinpaprika.com/v1`;
-
-export const fetchCoins = async () => {
-  return await (await fetch(`${BASE_URL}/coins`)).json();
-};
-
-// export const fetchCoinHistory = async (coinId: string) => {
-//   return await (
-//     await fetch(`https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`)
-//   ).json();
-// };
-
 // bithumb API
 
 const API_URL = `https://api.bithumb.com/public/`;
