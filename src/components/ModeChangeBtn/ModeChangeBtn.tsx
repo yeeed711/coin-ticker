e104d6297aa1b494aa3e707c948105353018ea9b
yeeed711 <rladpwl910@naver.com>
2022-09-30 16:54:24 +0900

    feat: ModeChangeBtn 개발

---
+++ src/components/ModeChangeBtn/ModeChangeBtn.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface IModeBtn {
+  onClick: () => void;
+  children: string;
+}
+
+const ModeChangeBtn = ({ onClick, children }: IModeBtn) => {
+  return <ModeBtn onClick={onClick}>{children}</ModeBtn>;
+};
+
+export default ModeChangeBtn;
+
+const ModeBtn = styled.button`
+  border-radius: 6px;
+  background-color: ${(props) => props.theme.color.bg.lv2};
+  box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.1);
+  height: 30px;
+  padding: 0.8rem 1.2rem;
+  font-size: 1.4rem;
+  transition: all 0.2s ease-in-out;
+  outline: 2px solid #619ef9;
+  &:hover {
+    color: ${(props) => props.theme.color.mode.lv2};
+    background-color: ${(props) => props.theme.color.mode.lv1};
+  }
+`;
