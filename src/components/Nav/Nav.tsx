7c32c5eb5bafed58ec465f5e35c4f647483d22fe
yeeed711 <rladpwl910@naver.com>
2022-11-05 23:41:45 +0900

    edit: nav style 분기 파일이름수정

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,23 +2,21 @@ import { Link } from 'react-router-dom';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { isDarkAtom } from '../../atoms';
 import ModeChangeBtn from '../ModeChangeBtn/ModeChangeBtn';
-import * as S from './style';
+import * as nav from './style';
 
 const Nav = () => {
-  const setDarkAtom = useSetRecoilState(isDarkAtom);
-  const toggleDarkAtom = () => setDarkAtom((prev) => !prev);
-  const isDark = useRecoilValue(isDarkAtom);
+    const setDarkAtom = useSetRecoilState(isDarkAtom);
+    const toggleDarkAtom = () => setDarkAtom((prev) => !prev);
+    const isDark = useRecoilValue(isDarkAtom);
 
-  return (
-    <S.Header>
-      <S.Logo>
-        <Link to='/'>{`{ COIN$ TRACKER }`}</Link>
-      </S.Logo>
-      <ModeChangeBtn onClick={toggleDarkAtom}>
-        {isDark ? 'Light' : 'Dark'}
-      </ModeChangeBtn>
-    </S.Header>
-  );
+    return (
+        <nav.Header>
+            <nav.Logo>
+                <Link to='/'>{`{ COIN$ TRACKER }`}</Link>
+            </nav.Logo>
+            <ModeChangeBtn onClick={toggleDarkAtom}>{isDark ? 'Light' : 'Dark'}</ModeChangeBtn>
+        </nav.Header>
+    );
 };
 
 export default Nav;
--- src/components/Nav/style.ts
+++ src/components/Nav/style.ts
@@ -1,15 +1,21 @@
 import styled from 'styled-components';
 
 export const Header = styled.header`
-  height: 10vh;
-  text-align: center;
-  padding: 2rem;
-  display: flex;
-  justify-content: space-between;
+    height: 10vh;
+    text-align: center;
+    padding: 20px;
+    display: flex;
+    justify-content: space-between;
+    gap: 2rem;
 `;
 
 export const Logo = styled.h1`
-  font-style: oblique;
-  font-weight: 500;
-  font-size: 4rem;
+    font-style: oblique;
+    font-weight: 500;
+    font-size: 4rem;
+    word-break: keep-all;
+    @media screen and (max-width: 390px) {
+        font-size: 3em;
+        flex-shrink: 1;
+    }
 `;
